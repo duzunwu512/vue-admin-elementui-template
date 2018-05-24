b8e78a52aa9d9aa3732e904e9d95f2d116db2407
duzunwu512 <94684873@qq.com>
2018-05-24 12:51:08 +0800

    初始上传可运行模板

---
+++ src/views/test/chart.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <line-chart :x-data="xData" :sData="sData"></line-chart>
+    <bar-chart :x-data="xData" :sData="sData" id="barchart1"></bar-chart>
+    <bar-chart :x-data="xData" :sData="sData2" id="barchart2"></bar-chart>
+  </div>
+</template>
+<script>
+import lineChart from '@/components/Chart/lineChart'
+import barChart from '@/components/Chart/barChart'
+export default {
+  components: { lineChart, barChart },
+  data () {
+    return {
+      xData: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
+      sData: [820, 932, 901, 934, 1290, 1330, 1320],
+      sData2: [1320,820, 932, 901, 934, 1290, 1330]
+    }
+  }
+}
+</script>
